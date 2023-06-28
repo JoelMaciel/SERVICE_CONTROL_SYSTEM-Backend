d592d16fe122494e418af7309b203e26b6abb4f1
Joel Maciel <soldado25461@gmail.com>
2023-06-28 16:30:07 -0300

    feat: inserting afterMigration to populate the database

--- src/main/java/joelmaciel/service_control/domain/exception/CpfAlreadyExistsException.java
+++ src/main/java/joelmaciel/service_control/domain/exception/CpfAlreadyExistsException.java
@@ -3,7 +3,7 @@ package joelmaciel.service_control.domain.exception;
 public class CpfAlreadyExistsException extends EntityInUseException {
 
     public CpfAlreadyExistsException(String cpf) {
-        super(String.format("There is already a client registered with this CPF :  %s  ", cpf));
+        super(String.format("There is already a client registered with this CPF : %s  ", cpf));
     }
 
 }
