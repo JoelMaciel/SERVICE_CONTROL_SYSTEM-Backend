e41557aa3da33f1d546223f61c53695b4ceb2f47
Joel Maciel <soldado25461@gmail.com>
2023-07-01 23:38:39 -0300

    refactor: refactoring the webConfig class referring to CORS

--- src/main/java/joelmaciel/service_control/api/dto/request/ClientRequestDTO.java
+++ src/main/java/joelmaciel/service_control/api/dto/request/ClientRequestDTO.java
@@ -1,9 +1,7 @@
 package joelmaciel.service_control.api.dto.request;
 
 import joelmaciel.service_control.domain.model.Client;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.constraints.NotBlank;
@@ -11,6 +9,8 @@ import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ClientRequestDTO {
 
